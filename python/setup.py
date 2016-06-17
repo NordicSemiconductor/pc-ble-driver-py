d6c0e6a9b4ff444e37b5e635a08de7f39073623d
Carles Cufi <carles.cufi@nordicsemi.no>
2016-06-17 14:57:16 +0200

    Update setup.py with correct info

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys
 
-import pynrfjprog
+import pc_ble_driver_py
 
 py2 = sys.version_info[0] == 2
 py3 = sys.version_info[0] == 3
@@ -21,19 +21,19 @@ setup(
     description = 'Python bindings for the Nordic pc-ble-driver SoftDevice serialization library',
     long_description = 'A Python interface and library for pc-ble-driver. Since the shared libraries are 32-bit applications, this package can only be used with 32-bit Python 2.7.x',
     
-    url = 'http://www.nordicsemi.com/',
+    url = 'https://github.com/NordicSemiconductor/pc-ble-driver-py',
         
     author = 'Nordic Semiconductor ASA',
     
-    license = 'BSD',
+    license = 'Modified BSD License',
     
     classifiers = [
 
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
 
         'Intended Audience :: Developers',
         
-        'Operating System :: MacOS',
+        'Operating System :: MacOS :: MacOSX',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
 
@@ -42,12 +42,11 @@ setup(
         'Topic :: Software Development :: Embedded Systems',
         
         'License :: Other/Proprietary License',
-        #'License :: OSI Approved :: BSD License',
 
         'Programming Language :: Python :: 2.7',
     ],
     
-    keywords = 'pc-ble-driver pc-ble-driver-py pc_ble_driver pc_ble_driver_py',
+    keywords = 'nordic nrf51 nrf52 ble bluetooth softdevice serialization bindings pc-ble-driver pc-ble-driver-py pc_ble_driver pc_ble_driver_py',
      
     install_requires = requirements,
      
