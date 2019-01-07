2415ed8d6f8ec806cfb5d3a3610c7753993a943e
Ken A. Redergård <64542+kenr@users.noreply.github.com>
2019-01-07 13:01:20 +0100

    Remove nose test target
    Remove nose test target since we will not be using nose anymore

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2016 Nordic Semiconductor ASA
+# Copyright (c) 2016-2019 Nordic Semiconductor ASA
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -34,10 +34,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
 import sys
+
+from setuptools import setup, find_packages
+
 import pc_ble_driver_py
 
 py2 = sys.version_info[0] == 2
@@ -47,30 +47,25 @@ if py2:
     requirements = ['enum34', 'wrapt', 'future']
 elif py3:
     requirements = ['wrapt', 'future']
-
-class NoseTestCommand(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import nose
-        nose.run_exit(argv=['nosetests', '--with-xunit', '--xunit-file=unittests.xml'])
+else:
+    print('pc-ble-driver-py only supports Python version 2 and 3')
+    sys.exit(-1)
 
 setup(
-    name ='pc_ble_driver_py',
-    version = pc_ble_driver_py.__version__,
-    description = 'Python bindings for the Nordic pc-ble-driver SoftDevice serialization library',
-    long_description = 'A Python interface and library for pc-ble-driver. This allows Python applications to interface with a Nordic Semiconductor IC (both nRF51 and nRF52 series) over a serial port to obtain access to the full serialized SoftDevice API.',
-    url = 'https://github.com/NordicSemiconductor/pc-ble-driver-py',
-    author = 'Nordic Semiconductor ASA',
-    license = 'Modified BSD License',
-    classifiers = [
+    name='pc_ble_driver_py',
+    version=pc_ble_driver_py.__version__,
+    description='Python bindings for the Nordic pc-ble-driver SoftDevice serialization library',
+    long_description='A Python interface and library for pc-ble-driver. This allows Python applications to interface '
+                     'with a Nordic Semiconductor IC (both nRF51 and nRF52 series) over a serial port to obtain '
+                     'access to the full serialized SoftDevice API.',
+    url='https://github.com/NordicSemiconductor/pc-ble-driver-py',
+    author='Nordic Semiconductor ASA',
+    license='Modified BSD License',
+    classifiers=[
         'Development Status :: 4 - Beta',
 
         'Intended Audience :: Developers',
-        
+
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
@@ -78,7 +73,7 @@ setup(
         'Topic :: System :: Networking',
         'Topic :: System :: Hardware :: Hardware Drivers',
         'Topic :: Software Development :: Embedded Systems',
-        
+
         'License :: Other/Proprietary License',
 
         'Programming Language :: Python',
@@ -90,25 +85,18 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
-    keywords = 'nordic nrf51 nrf52 ble bluetooth softdevice serialization bindings pc-ble-driver pc-ble-driver-py pc_ble_driver pc_ble_driver_py',
+    keywords='nordic nrf51 nrf52 ble bluetooth softdevice serialization bindings pc-ble-driver pc-ble-driver-py '
+             'pc_ble_driver pc_ble_driver_py',
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
-    install_requires = requirements,
-    tests_require=[
-        "nose >= 1.3.4",
-        "behave"
-    ],
-    packages = find_packages(),
-    package_data = {
-                'pc_ble_driver_py.lib.win.x86_32': ['*.pyd', '*.dll', '*.txt'],
-                'pc_ble_driver_py.lib.win.x86_64': ['*.pyd', '*.dll', '*.txt'],
-                'pc_ble_driver_py.lib.linux.x86_64': ['*.so', '*.txt'],
-                'pc_ble_driver_py.lib.macos_osx': ['*.so', '*.dylib', '*.txt'],
-                'pc_ble_driver_py.hex': ['*.hex', '*.patch', '*.sh'],
-                'pc_ble_driver_py.hex.sd_api_v2': ['*.hex'],
-                'pc_ble_driver_py.hex.sd_api_v3': ['*.hex'],
-                'pc_ble_driver_py.hex.sd_api_v5': ['*.hex'],
-    },
-    cmdclass={
-        'test': NoseTestCommand
+    install_requires=requirements,
+    packages=find_packages(),
+    package_data={
+        'pc_ble_driver_py.lib.win.x86_32': ['*.pyd', '*.dll', '*.txt'],
+        'pc_ble_driver_py.lib.win.x86_64': ['*.pyd', '*.dll', '*.txt'],
+        'pc_ble_driver_py.lib.linux.x86_64': ['*.so', '*.txt'],
+        'pc_ble_driver_py.lib.macos_osx': ['*.so', '*.dylib', '*.txt'],
+        'pc_ble_driver_py.hex': ['*.hex'],
+        'pc_ble_driver_py.hex.sd_api_v2': ['*.hex'],
+        'pc_ble_driver_py.hex.sd_api_v5': ['*.hex'],
     }
 )
