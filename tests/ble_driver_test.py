6b7bf39d51d49afd4cb8463fdfb213eeadb4311e
halkver <halvor-k@hotmail.com>
2019-10-25 14:17:49 +0200

    Remove duplicate code

--- tests/ble_driver_test.py
+++ tests/ble_driver_test.py
@@ -6,21 +6,22 @@ from pc_ble_driver_py.ble_driver import Flasher
 
 class FlasherParserTestCase(unittest.TestCase):
 
+    FW_STRUCT = Flasher.parse_fw_struct([
+        '17', 'A5', 'D8', '46',  # magic number
+        '02',  # struct version
+        'FF', 'FF', 'FF',  # (reserved for future use)
+        '00', '00', '00', '00',  # revision hash
+        '04', '01', '01',  # major, minor, patch
+        'FF',  # (reserved for future use)
+        '05',  # softdevice ble api number
+        '01',  # transport type
+        'FF', 'FF',  # (reserved for future use)
+        '40', '42', '0F', '00'  # baud rate
+    ])
+
     def test_invalid_parse(self):
-        fw_struct = Flasher.parse_fw_struct([
-            '17', 'A5', 'D8', '46',  # magic number
-            '02',  # struct version
-            'FF', 'FF', 'FF',  # (reserved for future use)
-            '00', '00', '00', '00',  # revision hash
-            '04', '01', '01',  # major, minor, patch
-            'FF',  # (reserved for future use)
-            '05',  # softdevice ble api number
-            '01',  # transport type
-            'FF', 'FF',  # (reserved for future use)
-            '40', '42', '0F', '00'  # baud rate
-        ])
         self.assertNotEqual(
-            fw_struct,
+            FlasherParserTestCase.FW_STRUCT,
             {
                 'len': 25,
                 'magic_number': ['17', 'A5', 'D8', '45'],
@@ -31,20 +32,8 @@ class FlasherParserTestCase(unittest.TestCase):
         )
 
     def test_valid_parse(self):
-        fw_struct = Flasher.parse_fw_struct([
-            '17', 'A5', 'D8', '46',  # magic number
-            '02',  # struct version
-            'FF', 'FF', 'FF',  # (reserved for future use)
-            '00', '00', '00', '00',  # revision hash
-            '04', '01', '01',  # major, minor, patch
-            'FF',  # (reserved for future use)
-            '05',  # softdevice ble api number
-            '01',  # transport type
-            'FF', 'FF',  # (reserved for future use)
-            '40', '42', '0F', '00'  # baud rate
-        ])
         self.assertEqual(
-            fw_struct,
+            FlasherParserTestCase.FW_STRUCT,
             {
                 'len': 24,
                 'magic_number': ['17', 'A5', 'D8', '46'],
