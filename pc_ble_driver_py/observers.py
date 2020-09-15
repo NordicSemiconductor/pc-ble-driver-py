cf49c9d5b648bdb531a0019e7ac25d598883c072
Bjørn Inge Hanssen <bihanssen@users.noreply.github.com>
2020-09-15 14:29:30 +0200

    Add on_indication_handle

--- pc_ble_driver_py/observers.py
+++ pc_ble_driver_py/observers.py
@@ -327,7 +327,10 @@ class BLEAdapterObserver(object):
     def __init__(self, *args, **kwargs):
         super(BLEAdapterObserver, self).__init__()
 
-    def on_indication(self, ble_adapter, conn_handle, uuid, char_handle, data):
+    def on_indication(self, ble_adpater, conn_handle, uuid, data):
+    pass
+
+    def on_indication_handle(self, ble_adapter, conn_handle, uuid, attr_handle, data):
         pass
 
     def on_notification(self, ble_adapter, conn_handle, uuid, char_handle, data):
