NordicSemiconductor/pc-ble-driver-py

commit 7d4bfbf041c1ad2868ee2a45ed45de2d07ffea7f

correct the versioning (#222)

setup.py
@@ -96,14 +96,14 @@ setup(
         "License :: Other/Proprietary License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="nordic nrf51 nrf52 ble bluetooth softdevice serialization bindings pc-ble-driver pc-ble-driver-py "
     "pc_ble_driver pc_ble_driver_py",
-    python_requires=">=3.6, <3.11",
+    python_requires=">=3.7, <3.11",
     install_requires=requirements,
     packages=packages,
     package_data={

commit 840f440800ab27a2058cec3e2938247386770d4d

Update the python version check to 3.7
The python version was bumped in #222 but missed one of the
checks so update this to check for 3.7 too.

Fixes: 7d4bfbf ("correct the versioning (#222)")
Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

setup.py
@@ -42,8 +42,8 @@ import os
 from skbuild import setup
 from setuptools import find_packages
 
-if sys.version_info < (3, 6):
-    print("pc-ble-driver-py only supports Python version 3.6 and newer")
+if sys.version_info < (3, 7):
+    print("pc-ble-driver-py only supports Python version 3.7 and newer")
     sys.exit(-1)
 
 requirements = ["wrapt", "cryptography"]